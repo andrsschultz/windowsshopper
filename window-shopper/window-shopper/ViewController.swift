11a7ecfd00879ea037a7a5bb231b225048b8bcd9
Andreas Schultz <andreasschultz@Andreass-MBP.fritz.box>
2018-12-19 23:37:27 +0100

    First commit

---
+++ window-shopper/window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Andreas Schultz on 19.12.18.
+//  Copyright © 2018 Andreas Schultz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
